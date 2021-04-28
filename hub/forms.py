58a0b261cb2ee2eb8cf452f7195a3032825325fd
Jakob Sejten <sejten97@gmail.com>
2021-04-28 11:26:32 +0200

    Cleaned databases

--- hub/forms.py
+++ hub/forms.py
@@ -3,84 +3,131 @@ from hub import app
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, SelectField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, NoneOf
+from wtforms import (
+    StringField,
+    PasswordField,
+    SubmitField,
+    BooleanField,
+    TextAreaField,
+    SelectField,
+)
+from wtforms.validators import (
+    DataRequired,
+    Length,
+    Email,
+    EqualTo,
+    ValidationError,
+    NoneOf,
+)
 from hub.models import User, Job
 
-class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up')
 
-    def validate_username(self,username):
+class RegistrationForm(FlaskForm):
+    username = StringField(
+        "Username", validators=[DataRequired(), Length(min=4, max=20)]
+    )
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirm_password = PasswordField(
+        "Confirm Password", validators=[DataRequired(), EqualTo("password")]
+    )
+    submit = SubmitField("Sign Up")
+
+    def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError('That username is taken. Please choose a different one.')
+            raise ValidationError(
+                "That username is taken. Please choose a different one."
+            )
 
-    def validate_email(self,email):
+    def validate_email(self, email):
         email = User.query.filter_by(email=email.data).first()
         if email:
-            raise ValidationError('That email is taken. Please use a different one.')
+            raise ValidationError("That email is taken. Please use a different one.")
+
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=20)])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember = BooleanField('Remember me')
-    submit = SubmitField('Login')
+    username = StringField(
+        "Username", validators=[DataRequired(), Length(min=4, max=20)]
+    )
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember = BooleanField("Remember me")
+    submit = SubmitField("Login")
 
 
 class UpdateAccountForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg','jpeg', 'png'])])
-    submit = SubmitField('Update')
+    username = StringField(
+        "Username", validators=[DataRequired(), Length(min=4, max=20)]
+    )
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    picture = FileField(
+        "Update Profile Picture", validators=[FileAllowed(["jpg", "jpeg", "png"])]
+    )
+    submit = SubmitField("Update")
 
-    def validate_username(self,username):
+    def validate_username(self, username):
         if username.data != current_user.username:
             user = User.query.filter_by(username=username.data).first()
             if user:
-                raise ValidationError('That username is taken. Please choose a different one.')
+                raise ValidationError(
+                    "That username is taken. Please choose a different one."
+                )
 
-    def validate_email(self,email):
+    def validate_email(self, email):
         if email.data != current_user.email:
             email = User.query.filter_by(email=email.data).first()
             if email:
-                raise ValidationError('That email is taken. Please use a different one.')
+                raise ValidationError(
+                    "That email is taken. Please use a different one."
+                )
 
-colorChoices = ['Blackizzle', 'Redizzle', 'Greenizzle', 'Bluezzle']
-materialChoices = ['PLA', 'ABS', 'PETG']
+
+colorChoices = ["Black", "Red", "Green", "Blue"]
+materialChoices = ["PLA", "ABS", "PETG"]
 
 
 def uploaded_files():
-    gcode_path = os.path.join(app.root_path, 'static/gcode_files')
+    gcode_path = os.path.join(app.root_path, "static/gcode_files")
     used_files = os.listdir(gcode_path)
     return used_files
 
 
 class JobForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    jobfile = FileField('Upload GCode File', validators=[FileAllowed(['gcode'])])
-    color = SelectField('Select Color', choices=colorChoices, validators=[DataRequired()])
-    material = SelectField('Select Material', choices=materialChoices,validators=[DataRequired()])
-    comment = TextAreaField('Comment')
-    qty = SelectField('Select Quantity', validators=[DataRequired()], choices=range(1,1000), coerce=int)
-    ignore_filename = BooleanField('Override currently uploaded file with same filename.', default=False)
-    submit = SubmitField('Submit')
-
+    title = StringField("Title", validators=[DataRequired()])
+    jobfile = FileField("Upload GCode File", validators=[FileAllowed(["gcode"])])
+    color = SelectField(
+        "Select Color", choices=colorChoices, validators=[DataRequired()]
+    )
+    material = SelectField(
+        "Select Material", choices=materialChoices, validators=[DataRequired()]
+    )
+    comment = TextAreaField("Comment")
+    qty = SelectField(
+        "Select Quantity",
+        validators=[DataRequired()],
+        choices=range(1, 1000),
+        coerce=int,
+    )
+    ignore_filename = BooleanField(
+        "Override currently uploaded file with same filename.", default=False
+    )
+    submit = SubmitField("Submit")
 
     def validate_jobfile(self, jobfile):
         if self.jobfile.data is None:
-            raise ValidationError('Choose a file to upload.')
+            raise ValidationError("Choose a file to upload.")
         elif jobfile.data.filename in uploaded_files():
             if not self.ignore_filename.data:
-                ignore_filename_message = 'A file with this name is already in the queue. Do you wish to replace the existing file? This will also replace the file for jobs that have previously been queued.'
+                ignore_filename_message = "A file with this name is already in the queue. Do you wish to replace the existing file? This will also replace the file for jobs that have previously been queued."
                 raise ValidationError(ignore_filename_message)
 
 
 class WorkerForm(FlaskForm):
-    name = StringField('Printer Name', validators=[DataRequired()])
-    color = SelectField('Select Color', choices=colorChoices, validators=[DataRequired()])
-    material = SelectField('Select Material', choices=materialChoices, validators=[DataRequired()])
-    submit = SubmitField('Submit')
+    name = StringField("Printer Name", validators=[DataRequired()])
+    color = SelectField(
+        "Select Color", choices=colorChoices, validators=[DataRequired()]
+    )
+    material = SelectField(
+        "Select Material", choices=materialChoices, validators=[DataRequired()]
+    )
+    submit = SubmitField("Submit")
--- hub/models.py
+++ hub/models.py
@@ -2,43 +2,62 @@ from datetime import datetime
 from hub import db, login_manager
 from flask_login import UserMixin
 
+
 @login_manager.user_loader
 def load_user(get_id):
     return User.query.get(int(get_id))
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    image_file = db.Column(db.String(20), nullable=False, default="default.jpg")
     password = db.Column(db.String(60), nullable=False)
-    jobs = db.relationship('Job', backref='user')
-    workers = db.relationship('Worker', backref='user')
+    jobs = db.relationship("Job", backref="user")
+    workers = db.relationship("Worker", backref="user")
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
-class Job(db.Model):
-    id = db.Column(db.Integer, primary_key=True) # Primary key for job
-    uploadID = db.Column(db.Integer, unique=False) # ID for uploads - used for quantity settings
-    groupID = db.Column(db.Integer, unique=False) # ID to group different jobs
-    queuePosition = db.Column(db.Integer) # Number used to sort job queue positions
-    title = db.Column(db.String, nullable=False) # Arbitrary name of the job given by user
-    code = db.Column(db.String, nullable=False) # Filename of uploaded GCode
-    color = db.Column(db.String, nullable=False) # Color of filament used
-    material = db.Column(db.String, nullable=False) # Filament material
-    datePosted = db.Column(db.DateTime, nullable=False, default=datetime.now()) # Date job is posted
-    datePrintStart = db.Column(db.DateTime, nullable=True) # Date job is initiated
-    datePrintFinish = db.Column(db.DateTime, nullable=True) # Date job is completed
-    qty = db.Column(db.Integer, nullable=False) # Job multiplier - this column will be deleted in later versions
-    comment = db.Column(db.Text, nullable=True) # Comment by user regarding job
-    status = db.Column(db.String, nullable=False) # Status of print - In queue, printed, completed, ect.
-    printerID = db.Column(db.Integer, db.ForeignKey('worker.id')) # ID used to assign job or refer job to the used printer
-    userID = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) # ID used to assign job or refer job to the used printer
 
+class Job(db.Model):
+    id = db.Column(db.Integer, primary_key=True)  # Primary key for job
+    uploadID = db.Column(
+        db.Integer, unique=False
+    )  # ID for uploads - used for quantity settings
+    groupID = db.Column(db.Integer, unique=False)  # ID to group different jobs
+    queuePosition = db.Column(db.Integer)  # Number used to sort job queue positions
+    title = db.Column(
+        db.String(20), nullable=False
+    )  # Arbitrary name of the job given by user
+    code = db.Column(db.String, nullable=False)  # Filename of uploaded GCode
+    color = db.Column(db.String, nullable=False)  # Color of filament used
+    material = db.Column(db.String, nullable=False)  # Filament material
+    datePosted = db.Column(
+        db.DateTime, nullable=False, default=datetime.now()
+    )  # Date job is posted
+    datePrintStart = db.Column(db.DateTime, nullable=True)  # Date job is initiated
+    datePrintFinish = db.Column(db.DateTime, nullable=True)  # Date job is completed
+    qty = db.Column(
+        db.Integer, nullable=False
+    )  # Job multiplier - this column will be deleted in later versions
+    comment = db.Column(db.Text, nullable=True)  # Comment by user regarding job
+    status = db.Column(
+        db.String, nullable=False
+    )  # Status of print - In queue, printed, completed, ect.
+    printerID = db.Column(
+        db.Integer, db.ForeignKey("worker.id")
+    )  # ID used to assign job or refer job to the used printer
+    userID = db.Column(
+        db.Integer, db.ForeignKey("user.id"), nullable=False
+    )  # ID used to assign job or refer job to the used printer
 
     def __repr__(self):
-        return "Job({}, {}, {}, {}, {}, {})".format(self.id, self.title, self.code,self.color, self.material, self.printerID)
+        return "Job({}, {}, {}, {}, {}, {})".format(
+            self.id, self.title, self.code, self.color, self.material, self.printerID
+        )
+
 
 class Worker(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -46,8 +65,10 @@ class Worker(db.Model):
     filamentColor = db.Column(db.String, nullable=False)
     filamentMaterial = db.Column(db.String, nullable=False)
     status = db.Column(db.String, nullable=False, default="Available")
-    assignedJobs = db.relationship('Job', backref='worker')
-    userID = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    assignedJobs = db.relationship("Job", backref="worker")
+    userID = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
 
     def __repr__(self):
-        return "Worker({}, {}, {}, {})".format(self.id, self.name, self.filamentColor, self.filamentMaterial)
+        return "Worker({}, {}, {}, {})".format(
+            self.id, self.name, self.filamentColor, self.filamentMaterial
+        )
